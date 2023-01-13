10e34742c24629a8cb7b3c0da6eec9a4ad3fef40
Serj-Melnyk <SerhiiMelnykGit@gmail.com>
2023-01-13 20:56:54 +0200

    hw-07.js-basicLightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,4 +5,38 @@ console.log(galleryItems);
 
 const gallery = document.querySelector(".gallery");
 
-const marcup = galleryItems(map url => `<li><img src="${}" alt=""></li>`);
+const marcup = galleryItems
+  .map(
+    (item) => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}"> 
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", marcup);
+
+gallery.addEventListener("click", onClick);
+function onClick(evt) {
+  console.log(evt);
+  evt.preventDefault();
+}
+
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `);
+
+// instance.show();
+
+const instance = basicLightbox.create(`
+<img src="${item.preview}" alt="${item.description}">
+`);
+instance.show();
+// // console.log(basicLightbox);
+// console.log(basicLightbox);
