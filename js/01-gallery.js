ee8bbaf86ae7f21851baeb2b2e3b8933a7afe8c2
Serj-Melnyk <SerhiiMelnykGit@gmail.com>
2023-01-13 18:44:37 +0200

    hw-07.js-marcup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,4 +5,19 @@ console.log(galleryItems);
 
 const gallery = document.querySelector(".gallery");
 
-const marcup = galleryItems(map url => `<li><img src="${}" alt=""></li>`);
+const marcup = galleryItems
+  .map(
+    (items) => `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>`
+  )
+  .join();
+
+gallery.insertAdjacentHTML("beforeend", marcup);
