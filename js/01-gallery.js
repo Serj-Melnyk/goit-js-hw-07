a7b9b866b2e74e7737504fbdd2871cf6e8ca4486
Serj-Melnyk <SerhiiMelnykGit@gmail.com>
2023-01-13 11:58:14 +0200

    hw-07.js-start

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,8 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const marcup = galleryItems(map url => `<li><img src="${}" alt=""></li>`);
