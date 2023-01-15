cc688afdf72ba44c25ae6edd584e3f4e34b9dbe4
Serj-Melnyk <SerhiiMelnykGit@gmail.com>
2023-01-15 08:56:42 +0200

    hw-07.js-task-1.1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -23,21 +23,60 @@ const marcup = galleryItems
 gallery.insertAdjacentHTML("beforeend", marcup);
 
 gallery.addEventListener("click", onClick);
+
 function onClick(evt) {
   console.log(evt.target);
   evt.preventDefault();
+  document.addEventListener("keydown", onEscape);
 
   const instance = basicLightbox.create(`
       <img src="${evt.target.dataset.source}" width="800" height="600">
   `);
+  onShow: (instance) => {
+    document.addEventListener("keydown", onEscape);
+  };
+  onClose: (instance) => {
+    document.removeEventListener("keydown", onEscape);
+  };
+
   instance.show();
+  function onEscape() {
+    if (evt.code === "Escape") {
+      return;
+    }
+  }
+
+  // instance.close(() => closeModal());
+  // instance.close(() => console.log("lightbox not visible anymore"));
+  // document.addEventListener("keydown", onEscape);
+  // function onEscape(evt) {
+  // document.removeEventListener("keydown", onEscape);
+  // }
 }
 
-document.addEventListener("keydown", onEscape);
-function onEscape(evt) {
-  document.removeEventListener("keydown", onEscape);
-}
+//............................................
 
-// function closeModal() {
-//   gallery.removeEventListener("click", closeModal);
-// }
+// const onGalleryImgClick = (event) => {
+//   event.preventDefault();
+//   if (event.target.nodeName !== "IMG") {
+//     return;
+//   }
+//   const carousel = basicLightbox.create(
+//     `      <img src="${event.target.dataset.source}" width="800" height="600">  `,
+//     {
+//       onShow: (carousel) => {
+//         document.addEventListener("keydown", onEscapeKeyDown);
+//       },
+//       onClose: (carousel) => {
+//         document.removeEventListener("keydown", onEscapeKeyDown);
+//       },
+//     }
+//   );
+//   carousel.show();
+//   function onEscapeKeyDown(event) {
+//     if (event.code !== "Escape") {
+//       return;
+//     }
+//     carousel.close();
+//   }
+// };
