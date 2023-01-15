c4f90297217552211995dfa86c58637f19b36da8
Serj-Melnyk <SerhiiMelnykGit@gmail.com>
2023-01-15 17:14:22 +0200

    hw-07.js.completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,8 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-// const document = document.querySelector("");
+// console.log(galleryItems);
+
 const gallery = document.querySelector(".gallery");
 
 const marcup = galleryItems
@@ -25,69 +25,24 @@ gallery.insertAdjacentHTML("beforeend", marcup);
 gallery.addEventListener("click", onClick);
 
 function onClick(evt) {
-  console.log(evt.target);
   evt.preventDefault();
 
-  const instance = basicLightbox.create(`
-      <img src="${evt.target.dataset.source}" width="800" height="600">
-  `);
+  const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}" width="800" height="600">`,
+    {
+      onShow: (instance) => {
+        document.addEventListener("keydown", onEscape);
+      },
+      onClose: (instance) => {
+        document.removeEventListener("keydown", onEscape);
+      },
+    }
+  );
   instance.show();
 
-  gallery.addEventListener("click", (evt) => {
+  function onEscape(evt) {
     if (evt.code === "Escape") {
-      // document.removeEventListener("keydown", onEscape);
       instance.close();
     }
-  });
-
-  // onShow: (instance) => {
-  //   onEscape;
-  // };
-  // onClose: (instance) => {
-  //   document.removeEventListener("keydown", onEscape);
-  // };
-
-  // instance.close(() => closeModal());
-  // instance.close(() => console.log("lightbox not visible anymore"));
-  // document.addEventListener("keydown", onEscape);
-  // function onEscape(evt) {
-  // document.removeEventListener("keydown", onEscape);
-  // }
+  }
 }
-
-// document.addEventListener("keydown", onEscape);
-
-// function onEscape(evt) {
-//   document.addEventListener("keydown", onEscape);
-//   console.log(evt);
-
-// function onEscape(evt) {}
-
-// instance.show();
-// }
-//............................................
-
-// const onGalleryImgClick = (event) => {
-//   event.preventDefault();
-//   if (event.target.nodeName !== "IMG") {
-//     return;
-//   }
-//   const carousel = basicLightbox.create(
-//     `      <img src="${event.target.dataset.source}" width="800" height="600">  `,
-//     {
-//       onShow: (carousel) => {
-//         document.addEventListener("keydown", onEscapeKeyDown);
-//       },
-//       onClose: (carousel) => {
-//         document.removeEventListener("keydown", onEscapeKeyDown);
-//       },
-//     }
-//   );
-//   carousel.show();
-//   function onEscapeKeyDown(event) {
-//     if (event.code !== "Escape") {
-//       return;
-//     }
-//     carousel.close();
-//   }
-// };
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,7 +1,7 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const gallery = document.querySelector(".gallery");
 
@@ -24,17 +24,7 @@ function onClick(evt) {
   evt.preventDefault();
 
   const lightbox = new SimpleLightbox(".gallery a", {
-    /* options */
+    captionsData: "alt",
+    captionDelay: 250,
   });
 }
-
-/* <div class="gallery__item">
-  <a class="gallery__link" href="${item.original}">
-    <img
-      class="gallery__image"
-      src="${item.preview}"
-      data-source="${item.original}"
-      alt="${item.description}"
-    />
-  </a>
-</div>; */
